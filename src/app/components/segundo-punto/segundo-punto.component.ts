5d944ee1174cdc902f11c128207ce0e5c883b6ac
Santidev21 <orsantiago21@gmail.com>
2025-09-29 22:48:02 -0500

    Se corrigio la funcionalidad del boton de reproducir audio.

--- src/app/components/segundo-punto/segundo-punto.component.ts
+++ src/app/components/segundo-punto/segundo-punto.component.ts
@@ -2,19 +2,34 @@ import { Component } from '@angular/core';
 import { PreguntasContainerComponent } from '../preguntas-container/preguntas-container.component';
 import { RespuestaService } from '../../services/respuesta.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-segundo-punto',
-  imports: [PreguntasContainerComponent],
+  imports: [PreguntasContainerComponent, CommonModule],
   standalone: true,
   templateUrl: './segundo-punto.component.html',
   styleUrls: ['./segundo-punto.component.scss']
 })
 export class SegundoPuntoComponent {
+  isPlaying = false;
+
   constructor(private respuestaService: RespuestaService, private router: Router) {}
 
   onResponder(respuesta: string) {
     this.respuestaService.setRespuesta('P2', respuesta);
     this.router.navigate(['/navegacion/resolucion']);
+  }
+
+  onPlay() {
+    this.isPlaying = true;
+  }
+
+  onPause() {
+    this.isPlaying = false;
+  }
+
+  onEnded() {
+    this.isPlaying = false;
   }
 }
